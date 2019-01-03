fa41007b30730fbb90c5dbcae80d6bfa0c0f4b48
Campbell Affleck <cbaffleck@berkeley.edu>
2019-01-02 18:27:11 -0800

    Can now read user input, and secure text entry is turned on for password field

--- SafeWalk/SafeWalk/ViewController.swift
+++ SafeWalk/SafeWalk/ViewController.swift
@@ -9,22 +9,56 @@
 import UIKit
 import QuartzCore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var appNameLabel: UILabel!
     @IBOutlet weak var passTextField: UITextField!
     
+    //MARK: Variables
+    var userEmail = ""
+    var userPass = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks. Delegate is the viewcontroller
+        emailTextField.delegate = self
+        passTextField.delegate = self
+        
+        //Text box color adjustment
         emailTextField.layer.borderWidth = 1
         emailTextField.layer.borderColor = UIColor.init(red: 210/255.00, green: 210/255.00, blue: 210/255.00, alpha: 1.0).cgColor
         passTextField.layer.borderWidth = 1
         passTextField.layer.borderColor = UIColor.init(red: 210/255.00, green: 210/255.00, blue: 210/255.00, alpha: 1.0).cgColor
     }
-
+    
+    //MARK: UITextDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //When the textfield being edited is ready to be read, set the corresponding variable's text to the user input
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == emailTextField {
+            userEmail = String(textField.text!)
+        } else if textField == passTextField {
+            userPass = String(textField.text!)
+        }
+        print(userEmail + " " + userPass)
+    }
+    
+    //MARK: Actions
+    @IBAction func signIn(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotPassword(_ sender: UIButton) {
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+    }
+    
 
 }
 
