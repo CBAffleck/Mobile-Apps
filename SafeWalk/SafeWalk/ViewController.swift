9598326a31524789f2d747bd49d429d338d03780
Campbell Affleck <cbaffleck@berkeley.edu>
2019-01-02 16:42:33 -0800

    Added some code to adjust text box color. Added forgot pass and sign up buttons

--- SafeWalk/SafeWalk/ViewController.swift
+++ SafeWalk/SafeWalk/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var passTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.borderColor = UIColor.init(red: 210/255.00, green: 210/255.00, blue: 210/255.00, alpha: 1.0).cgColor
+        passTextField.layer.borderWidth = 1
+        passTextField.layer.borderColor = UIColor.init(red: 210/255.00, green: 210/255.00, blue: 210/255.00, alpha: 1.0).cgColor
     }
 
 
