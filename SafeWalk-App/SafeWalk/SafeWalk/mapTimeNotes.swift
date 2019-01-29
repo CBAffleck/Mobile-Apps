5e369bcc64bf4e88de28d651f112db73a94735d0
Campbell Affleck <cbaffleck@berkeley.edu>
2019-01-28 18:42:35 -0800

    Added screens for next steps in map storyboard.

---
+++ SafeWalk-App/SafeWalk/SafeWalk/mapTimeNotes.swift
@@ -0,0 +1,39 @@
+//
+//  mapTimeNotes.swift
+//  SafeWalk
+//
+//  Created by Campbell Affleck on 1/28/19.
+//  Copyright © 2019 Campbell Affleck. All rights reserved.
+//
+
+import UIKit
+
+class mapTimeNotes: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var setTimeNotesButton: UIButton!
+    
+    //MARK: Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: Actions
+    @IBAction func setTimeNotes(_ sender: UIButton) {
+    }
+    
+}
