c436b59f086c9563a3c7fa69f64fc919467d1cf8
Campbell Affleck <cbaffleck@berkeley.edu>
2019-01-30 17:59:02 -0800

    Created custom segue file. Set app to only be portrait mode

---
+++ SafeWalk-App/SafeWalk/SafeWalk/Custom Segues/SlideUpSegue.swift
@@ -0,0 +1,31 @@
+//
+//  SlideUpSegue.swift
+//  SafeWalk
+//
+//  Created by Campbell Affleck on 1/30/19.
+//  Copyright © 2019 Campbell Affleck. All rights reserved.
+//
+
+import UIKit
+
+class SlideUpSegue: UIStoryboardSegue {
+
+//    override func perform() {
+//        <#code#>
+//    }
+//    
+//    func slideUp() {
+//        let toViewController = self.destination
+//        let fromViewController = self.source
+//        let containerView = fromViewController.view.superview
+//        let originalBottom = fromViewController.view.bottomAnchor
+//        
+//        containerView?.addSubview(toViewController.view)
+//        
+//        UIView.animate(withDuration: 0.5, animations: <#T##() -> Void#>)
+//    }
+//    
+//    func slideDown() {
+//        
+//    }
+}
