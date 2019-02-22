67bb56bcf497295f5260a97191d52793bdebc2d5
Campbell Affleck <cbaffleck@berkeley.edu>
2019-02-21 17:47:41 -0800

    Added edit profile pic button image. Added edit profile screen and swift file. Linked edit screen to profile screen.

---
+++ SafeWalk-App/SafeWalk/SafeWalk/editProfileScreen.swift
@@ -0,0 +1,28 @@
+//
+//  editProfileScreen.swift
+//  SafeWalk
+//
+//  Created by Campbell Affleck on 2/21/19.
+//  Copyright © 2019 Campbell Affleck. All rights reserved.
+//
+
+import UIKit
+import AWSAuthCore
+import AWSMobileClient
+
+class editProfileScreen: UIViewController {
+
+    //MARK: Properties
+    
+    
+    //MARK: Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: Actions
+    
+}
